ff97a0896aa7ce2238b54e2b640c8f6f97cf575b
Vincent Lamicela <vtl9301@rit.edu>
2017-03-26 20:38:40 -0400

    Assignment 7: SLERP

---
+++ A07_SLERP/AppClass.cpp
@@ -0,0 +1,166 @@
+#include "AppClass.h"
+void AppClass::InitWindow(String a_sWindowName)
+{
+	super::InitWindow("SLERP - Vincent Lamicela"); // Window Name
+
+	//Setting the color to black
+	m_v4ClearColor = vector4(0.0f);
+}
+
+void AppClass::InitVariables(void)
+{
+	//Setting the position in which the camera is looking and its interest point
+	m_pCameraMngr->SetPositionTargetAndView(vector3(12.12f, 28.52f, 11.34f), ZERO_V3, REAXISY);
+
+	//Setting the color to black
+	m_v4ClearColor = vector4(0.0f);
+
+	//Loading the models
+	m_pMeshMngr->LoadModel("Planets\\00_Sun.obj", "Sun");
+	m_pMeshMngr->LoadModel("Planets\\03_Earth.obj", "Earth");
+	m_pMeshMngr->LoadModel("Planets\\03A_Moon.obj", "Moon");
+
+	//Setting the days duration
+	m_fDay = 1.0f;
+}
+
+void AppClass::Update(void)
+{
+	//Update the system's time
+	m_pSystem->UpdateTime();
+
+	//Update the mesh manager's time without updating for collision detection
+	m_pMeshMngr->Update();
+
+	//First person camera movement
+	if (m_bFPC == true)
+		CameraRotation();
+
+	//Getting the time between calls
+	double fCallTime = m_pSystem->LapClock();
+	//Counting the cumulative time
+	static double fRunTime = 0.0f;
+	fRunTime += fCallTime;
+
+	//Earth Orbit
+	double fEarthHalfOrbTime = 182.5f * m_fDay; //Earths orbit around the sun lasts 365 days / half the time for 2 stops
+	float fEarthHalfRevTime = 0.5f * m_fDay; // Move for Half a day
+	float fMoonHalfOrbTime = 14.0f * m_fDay; //Moon's orbit is 28 earth days, so half the time for half a route
+
+	//Create quaternions
+	quaternion qA = glm::angleAxis(0.0f, vector3(0.0f, 1.0f, 0.0f));
+	quaternion qB = glm::angleAxis(359.0f, vector3(0.0f, 1.0f, 0.0f));
+	quaternion qEarthO, qEarthR, qMoonO;
+	//Create percentages
+	float percEarth = MapValue((float)fRunTime, 0.0f, (float)fEarthHalfOrbTime, 0.0f, 1.0f) / 2;
+	float percEarthRev = MapValue((float)fRunTime, 0.0f, fEarthHalfRevTime, 0.0f, 1.0f) / 2;
+	float percMoon = MapValue((float)fRunTime, 0.0f, fMoonHalfOrbTime, 0.0f, 1.0f) / 2;
+	//SLERP the quaternions
+	qEarthO = glm::mix(qA, qB, percEarth);
+	qEarthR = glm::mix(qA, qB, percEarthRev);
+	qMoonO = glm::mix(qA, qB, percMoon);
+
+	//Cast quats to mats
+	matrix4 m4EarthO = glm::mat4_cast(qEarthO);
+	matrix4 m4EarthR = glm::mat4_cast(qEarthR);
+	matrix4 m4MoonO = glm::mat4_cast(qMoonO);
+
+	//Set the size and distance transformations
+	static matrix4 distanceEarth = glm::translate(11.0f, 0.0f, 0.0f);
+	static matrix4 distanceMoon = glm::translate(2.0f, 0.0f, 0.0f);
+	static matrix4 sizeSun = glm::scale(5.936f, 5.936f, 5.936f);
+	static matrix4 sizeEarth = glm::scale(0.524f, 0.524f, 0.524f);
+	static matrix4 sizeMoon = glm::scale(0.27f, 0.27f, 0.27f);
+
+	//Setting the matrices
+	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Sun");
+	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Earth");
+	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "Moon");
+	
+	//Set the position and scale of objects
+	matrix4 m4Sun = IDENTITY_M4 * sizeSun;
+
+	matrix4 m4Earth = m4EarthO * distanceEarth * sizeEarth;
+	
+	matrix4 m4Moon = m4Earth * m4MoonO * distanceMoon * sizeEarth * sizeMoon;
+	
+	m4Earth *= m4EarthR;
+	m_pMeshMngr->SetModelMatrix(m4Sun, "Sun");
+	m_pMeshMngr->SetModelMatrix(m4Earth, "Earth");
+	m_pMeshMngr->SetModelMatrix(m4Moon, "Moon");
+
+	//Adds all loaded instance to the render list
+	m_pMeshMngr->AddInstanceToRenderList("ALL");
+
+	int nEarthOrbits = (int)percEarth;
+	int nEarthRevolutions = (int)percEarthRev;
+	int nMoonOrbits = (int)percMoon;
+	//Increment these numbers for every respective orbit/rev
+	/*while (percMoon > 1.0f) {
+		percMoon = 0.0f;
+		nMoonOrbits++;
+	}
+	while (percEarthRev > 1.0f) {
+		percEarthRev = 0.0f;
+		nEarthRevolutions++;
+	}
+	while (percEarth > 1.0f) {
+		percEarth = 0.0f;
+		nEarthOrbits++;
+	}*/
+
+	//Indicate the FPS
+	int nFPS = m_pSystem->GetFPS();
+
+	//Print info on the screen
+	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), REYELLOW);
+	
+	m_pMeshMngr->Print("Time:");
+	m_pMeshMngr->PrintLine(std::to_string(fRunTime));
+
+	m_pMeshMngr->Print("Day:");
+	m_pMeshMngr->PrintLine(std::to_string(m_fDay));
+
+	m_pMeshMngr->Print("E_Orbits:");
+	m_pMeshMngr->PrintLine(std::to_string(nEarthOrbits));
+
+	m_pMeshMngr->Print("E_Revolutions:");
+	m_pMeshMngr->PrintLine(std::to_string(nEarthRevolutions));
+
+	m_pMeshMngr->Print("M_Orbits:");
+	m_pMeshMngr->PrintLine(std::to_string(nMoonOrbits));
+
+	m_pMeshMngr->Print("FPS:");
+	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
+}
+
+void AppClass::Display(void)
+{
+	//clear the screen
+	ClearScreen();
+
+	//Render the grid based on the camera's mode:
+	switch (m_pCameraMngr->GetCameraMode())
+	{
+	default:
+		break;
+	case CAMERAMODE::CAMROTHOX:
+		m_pMeshMngr->AddGridToRenderList(1.0f, REAXIS::YZ, RERED * 0.75f); //renders the YZ grid with a 100% scale
+		break;
+	case CAMERAMODE::CAMROTHOY:
+		m_pMeshMngr->AddGridToRenderList(1.0f, REAXIS::XZ, REGREEN * 0.75f); //renders the XZ grid with a 100% scale
+		break;
+	case CAMERAMODE::CAMROTHOZ:
+		m_pMeshMngr->AddGridToRenderList(1.0f, REAXIS::XY, REBLUE * 0.75f); //renders the XY grid with a 100% scale
+		break;
+	}
+	
+	m_pMeshMngr->Render(); //renders the render list
+	m_pMeshMngr->ClearRenderList();
+	m_pGLSystem->GLSwapBuffers(); //Swaps the OpenGL buffers
+}
+
+void AppClass::Release(void)
+{
+	super::Release(); //release the memory of the inherited fields
+}
--- E11_MidtermPractice/AppClass.cpp
+++ E11_MidtermPractice/AppClass.cpp
@@ -16,6 +16,40 @@ void AppClass::Update(void)
 #pragma endregion
 #pragma region YOUR CODE GOES HERE
 	m_m4Steve = glm::mat4(1.0f); // same as m_m4Steve = IDENTITY_M4; setting the identity to steve
+	static DWORD timeSinceBoot = GetTickCount(); //time since bootup
+	DWORD timeSinceStart = GetTickCount() - timeSinceBoot; //time since program started
+	float fTime = timeSinceStart / 1000.0f; //in milliseconds
+	std::vector<vector3> traPath; //translation path
+	traPath.push_back(vector3(0.0f, 0.0f, 0.0f)); //base
+	traPath.push_back(vector3(0.0f, 5.0f, 0.0f)); //top
+
+	int flagT = 0; //tracks point in translate path
+	int nextT = flagT + 1 % (traPath.size());
+	vector3 tStart = traPath[flagT]; //point A
+	vector3 tEnd = traPath[nextT]; //point B
+	vector3 tCurrent = vector3(0, 0, 0); //progress from A to B
+	//vector3 tPos = vector3(0, 0, 0);
+
+	float perc = MapValue(fTime, 0.0f, 5.0f, 0.0f, 1.0f); //percentage from A to B
+	while (perc > 1.0f) {
+		//reset percentage, seek next flag
+		perc -= 1.0f;
+		flagT++;
+		if (flagT > traPath.size() - 1) {
+			flagT = 0;
+		}
+		nextT = flagT + 1;
+		if (nextT > traPath.size() - 1) {
+			nextT = 0;
+		}
+	}
+	tStart = traPath[flagT]; //new point A
+	tEnd = traPath[nextT]; //new point B
+	tCurrent = glm::lerp(tStart, tEnd, perc); //distance from point A to B
+
+	m_pMeshMngr->SetModelMatrix(m_m4Steve, "Steve");
+	m_m4Steve = glm::rotate(IDENTITY_M4, fTime * 72, vector3(0.0f, 0.0f, 1.0f)) * glm::translate(tCurrent); //rotate and translate the Steve
+
 #pragma endregion
 #pragma region DOES NOT NEED CHANGES
 	//Set the model matrix
